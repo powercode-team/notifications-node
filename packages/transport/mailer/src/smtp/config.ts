af4dbf6717f46a0ef095229420b79ca37b4f61b5
and.xdev <and.xdev@gmail.com>
2023-03-27 16:57:00 +0300

    Debug

--- packages/transport/mailer/src/smtp/config.ts
+++ packages/transport/mailer/src/smtp/config.ts
@@ -3,13 +3,13 @@ import { ISmtpTransportConfig } from '@node-notifications/transport-mailer';
 export const configSmtp: ISmtpTransportConfig = {
   options: {
     host: process.env.MAIL_HOST,
-    port: Number(process.env.MAIL_PORT) || undefined,
+    port: Number(process.env.MAIL_PORT) || 1025,
     auth: {
       user: process.env.MAIL_USER,
       pass: process.env.MAIL_PASS,
     },
   },
   defaults: {
-    from: process.env.MAIL_FROM,
+    from: process.env.MAIL_FROM || '',
   },
 };
